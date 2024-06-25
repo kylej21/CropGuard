5018ff086bd62cd83107ff3c314fb2f6f060faaa
kylej21 <kylecj21@gmail.com>
2024-06-25 10:06:19 -0700

    Added vercel analytics

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import "@radix-ui/themes/styles.css";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
