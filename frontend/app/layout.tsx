4bb8cc3697e6f4c280dcf6a8a9f8987bdc4d78db
kylej21 <kylecj21@gmail.com>
2024-06-26 10:49:27 -0700

    Added analytics

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -18,7 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}
+        <Analytics/>
+      </body>
     </html>
   );
 }
