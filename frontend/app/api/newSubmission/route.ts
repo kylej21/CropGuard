cb3d68f44195377b64f41951a8e422d445f6577f
kylej21 <kylecj21@gmail.com>
2024-08-10 15:21:46 -0700

    updated serverless route for upload

--- frontend/app/api/newSubmission/route.ts
+++ frontend/app/api/newSubmission/route.ts
@@ -3,18 +3,25 @@ import { NextRequest, NextResponse } from "next/server";
 import { sql } from '@vercel/postgres';
 import { unstable_noStore as noStore } from 'next/cache';
 
-const fetchDescription = async () => {
+const fetchDescription = async (body: any) => {
     // Gemini description + processing of the image
+
+    const payload = fetch('http://127.0.0.1:5000/upload/', {
+        method: 'POST',
+        body: body
+    })
+    
+    const values = await (await payload).json();
     // for now return Tomato Blight
-    return ["Tomato Blight","Late Tomato Blight Late tomato blight is caused by..."];
+    return [values.status,values.explanation];
 }
 export async function POST(request: NextRequest){
     noStore();
-
     const body = await request.json();
+    console.log(body)
     const client = await sql.connect();
     console.log('add submission: '+ body.username)
-    const [response,description] = await fetchDescription() // <- put image as param later
+    const [response,description] = await fetchDescription(body) // <- put image as param later
     console.log(`${body.username}, ${response}, ${description}`)
     try {
         await client.sql`INSERT INTO submissions (username,result,description) VALUES (${body.username}, ${response}, ${description})`;
